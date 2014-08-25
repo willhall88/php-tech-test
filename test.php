d8463cf2c6937d42dc7e6befa4879fdde6a1fffb
willhall88 <willhall88@hotmail.com>
2014-08-25 14:18:06 +0100

    Now targeted the correct table using XPath. the data now needs to be processed to be read by the graphing library

--- test.php
+++ test.php
@@ -6,12 +6,18 @@
   <?php
   	ini_set('display_errors',1);  
 		error_reporting(E_ALL);
-		
+
   	$curl_handler = curl_init("http://en.wikipedia.org/wiki/List_of_highest-grossing_films");
 	  curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
 	  $wiki_page = curl_exec($curl_handler);
 
-	  echo $wiki_page;
+	  $dom = new DOMDocument();
+	  @$dom->loadHTML($wiki_page);
+	  $xpath = new DOMXpath($dom);
+	  
+	  $table = $xpath->query("//*[@id='mw-content-text']/table[1]//td");
+
+	  print_r($table);
 
   ?>
 </body>
