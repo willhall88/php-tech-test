a28ff42e25433a549c37a31e770cbcd4fe6a5e39
willhall88 <willhall88@hotmail.com>
2014-08-25 18:12:02 +0100

    refactored some code to create some private methods

--- src/pageScraper.php
+++ src/pageScraper.php
@@ -14,17 +14,28 @@ class PageScraper
       
       foreach ($this->xml->xpath($xpath_query) as $count => $row) {
           $cells = $row->td;
-          $title_cell = $row->th->i->a;
-          $formatted_revenue = str_replace(array('$', ','), '' , $cells[1]);
+      
           $films[] = array(
-          	'title' => (String)$title_cell,
-            'revenue' => (Integer)$formatted_revenue,
+          	'title' => $this->getTitle($row),
+            'revenue' => $this->formatCurrency($cells[1]),
             'year' => (String)$cells[2],
           );
       }
 
       array_shift($films);
 			return $films;
+    }
+
+    private function formatCurrency($currency)
+    {
+    	$formatted_currency = (Integer)str_replace(array('$', ','), '' , $currency);
+    	return $formatted_currency;
+    }
+
+    private function getTitle($row)
+    {
+    	$title = (String)$row->th->i->a;
+    	return $title;
     }
 
 }
