4961c795dadeccb86c69146c4d5c93b7d5bc6109
willhall88 <willhall88@hotmail.com>
2014-08-25 14:10:29 +0100

    pulled wiki page onto our test page

--- test.php
+++ test.php
@@ -4,7 +4,15 @@
 </head>
 <body>
   <?php
-  	echo 'hello';
+  	ini_set('display_errors',1);  
+		error_reporting(E_ALL);
+		
+  	$curl_handler = curl_init("http://en.wikipedia.org/wiki/List_of_highest-grossing_films");
+	  curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
+	  $wiki_page = curl_exec($curl_handler);
+
+	  echo $wiki_page;
+
   ?>
 </body>
 </html>
